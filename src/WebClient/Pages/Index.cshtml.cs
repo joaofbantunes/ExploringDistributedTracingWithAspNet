7cc16dd9a80299922c78e5e74f8b88c7b2f41577
João Antunes <joao@antunes.dev>
2019-11-26 17:32:38 +0000

    Playing with some explicit tracing

--- src/WebClient/Pages/Index.cshtml.cs
+++ src/WebClient/Pages/Index.cshtml.cs
@@ -1,25 +1,40 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
 
 namespace WebClient.Pages
 {
     public class IndexModel : PageModel
     {
-        public IndexModel()
+        private readonly ITracer _tracer;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(TracerFactoryBase tracerFactory, ILogger<IndexModel> logger)
         {
+            _tracer = tracerFactory.GetTracer(null); //use default tracer
+            _logger = logger;
         }
 
         [BindProperty]
         public InputModel Input { get; set; }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-
+            await DoSomeActivityAsync();
         }
-
+        
         public IActionResult OnPost() => RedirectToPage("hello", new { username = Input.Username });
 
+        private async Task DoSomeActivityAsync()
+        {
+            using var span = _tracer.StartActiveSpan("SomeActivity", out _);
+            _logger.LogInformation("Doing some activity, just for tracing!");
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+        }
 
 
         public class InputModel
