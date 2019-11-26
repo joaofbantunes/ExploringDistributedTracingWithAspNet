f41c94c281d490e3c0136adc04f2dc48f083f728
João Antunes <joao@antunes.dev>
2019-11-26 16:46:30 +0000

    Remove unused usings

--- src/WebApi/Controllers/HelloController.cs
+++ src/WebApi/Controllers/HelloController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using static GrpcService.Greeter;
 
 namespace WebApi.Controllers
--- src/WebClient/Pages/Index.cshtml.cs
+++ src/WebClient/Pages/Index.cshtml.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace WebClient.Pages
 {
-
-
     public class IndexModel : PageModel
     {
         public IndexModel()
